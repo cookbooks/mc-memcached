904f9cbfa6d07253e0ab15787419e5b29ed60752
Modcloth Operations <ops@ops-workstation-1.local>
2012-09-25 16:46:53 -0700

    Use 80% of memory by default if ohai memory attribute exists

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,10 @@
-default[:memcached][:memory] = 64
+if node['memory'] && node['memory']['total']
+  # Set memory to 80% of total
+  default[:memcached][:memory] = (node['memory']['total'] * 0.8).ceil
+else
+  default[:memcached][:memory] = 64
+end
+
 default[:memcached][:port] = 11211
 default[:memcached][:user] = "nobody"
 default[:memcached][:listen] = "0.0.0.0"
